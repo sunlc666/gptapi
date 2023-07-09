f2a5f7f13b34dbe178a3c41090c8952b064113c4
sunlc666 <102209415+sunlc666@users.noreply.github.com>
2023-07-10 00:17:15 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,12 @@
-export 默认 {
+export default {
   async fetch(request, env) {
-    const url = 新建 URL(request.url);
+    const url = new URL(request.url);
     url.host = "api.openai.com";
     // openai is already set all CORS heasders 
     return fetch(url, {
       headers: request.headers,
       method: request.method,
-      内容: request.内容,
+      body: request.body,
       redirect: 'follow'
     });
   }
